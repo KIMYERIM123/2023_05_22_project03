4741170ccc77159abb314114b6856b9c9cbcf414
KIMYERIM123 <133613841+KIMYERIM123@users.noreply.github.com>
2023-05-22 23:41:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 $(function () {
     $('.main_slide').slick({
-        // arrows: false,
-        autoplay: true,
+        arrows: false,
+        autoplay : true,
+        vertical: true,
+        autoplaySpeed :3000,
         pauseOnHover: false,
         pauseOnFocus: false,
-        autoplaySpeed: 3000,
+        dots : true
     });
+
+    $(window).on('scroll', function () {
+        // 변수에 스크롤한 양을 담는다
+        let sct = $(window).scrollTop();
+        console.log(sct);
+        if (sct > 0) {
+            $('header').addClass('on')
+        } else {
+            $('header').removeClass('on')
+        }
+
+    });
+
 })
